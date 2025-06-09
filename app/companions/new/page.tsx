9e5f250307534e2dfdfb40a8c74848edfdf624a3
Adil Imran <devadem1370@gmail.com>
2025-06-09 08:54:46 -0700

    implement clerk billing

--- app/companions/new/page.tsx
+++ app/companions/new/page.tsx
@@ -3,22 +3,40 @@ import {redirect} from "next/navigation";
 import Image from "next/image";
 import Link from "next/link";
 import { auth } from "@clerk/nextjs/server";
+import { newCompanionPermissions } from "@/lib/actions/companion.action";
 
 const NewCompanion = async () => {
    
     const {userId} = await auth();
 
-    if(!userId) redirect('/sign-in')
+    if(!userId) redirect('/sign-in');
+     
+    const canCreateCompanion = await newCompanionPermissions();
 
 
     return (
         <main className="min-lg:w-1/3 min-md:w-2/3 items-center justify-center pb-6">
             
-                <article className="w-full gap-4 flex flex-col">
+               { canCreateCompanion ? (<article className="w-full gap-4 flex flex-col">
+
                     <h1>Companion Builder</h1>
 
                     <CompanionForm />
-                </article>
+                </article>): (
+                    <article className="companion-limit">
+                        <Image src="/images/limit.svg" alt="Companion limit reached" width={360} height={230} />
+                        <div className="cta-badge">
+                            Upgrade your plan
+                        </div>
+                        <h1>You've Reached Your Limit
+
+                        </h1>
+                        <p>You've reached your limit, Upgarde to create more companions and premium features</p>
+                        <Link href='/subscription' className="btn-primary w-full justify-center">
+                        Upgrade My Plan
+                        </Link>
+                    </article>
+                )} 
                
                 
         </main>
