6be61578b7cf52e4eeb48fff369c1feabf32e2a3
Adil Imran <devadem1370@gmail.com>
2025-06-09 04:02:23 -0700

    implement recent sessions to homepage

--- app/page.tsx
+++ app/page.tsx
@@ -2,44 +2,39 @@ import CompanionCard from '@/components/CompanionCard'
 import React from 'react'
 import CompanionsList from '@/components/CompanionsList'
 import Cta from '@/components/Cta'
-import { recentSessions } from '@/constants'
+import { recentSessions, subjects } from '@/constants'
+import { getAllCompanions, getRecentSessions } from '@/lib/actions/companion.action'
+import { getSubjectColor } from '@/lib/utils'
 
-const Page = () => {
+const Page = async () => {
+
+  const companions = await getAllCompanions({limit: 3}) ;
+
+  const recentSessionsCompanion = await getRecentSessions( 10);
+
   return (
     <main className=''>
       <h1>Popular Companions</h1>
       <section className="home-section">
-        <CompanionCard
-        id="1"
-        name="Neura the Brainy Explorer"
-        topic="Neural Network of the Brain"
-        subject="Science"
-        duration={45}
-        color="#008903"
+        {companions.map((companion) =>(
+           <CompanionCard
+           {...companion}
+           key={companion.id}
+           color={getSubjectColor(companion.subject)}
+
+      
         />
-        <CompanionCard
-        id="3"
-        name="Neura the Brainy Explorer"
-        topic="Neural Network of the Brain"
-        subject="Science"
-        duration={90}
-        color="#9099ed"
-        />
-        <CompanionCard
-        id="12"
-        name="Neura the Brainy Explorer"
-        topic="Neural Network of the Brain"
-        subject="Science"
-        duration={40}
-        color="#980432"
-        />
+
+        ))}
+       
+       
         
       </section>
 
       <section className="home-section pb-3">
         <CompanionsList
         title="Recently completed sessions"
-        companions={recentSessions}
+        companions={recentSessionsCompanion}
         classNames="w-2/3 max-lg:w-full"
         
         />
