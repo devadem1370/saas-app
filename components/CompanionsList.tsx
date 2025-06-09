f58493c7a015cfc426ab568bde5724587d354d0f
Adil Imran <devadem1370@gmail.com>
2025-06-09 08:03:11 -0700

    create my journey page

--- components/CompanionsList.tsx
+++ components/CompanionsList.tsx
@@ -20,7 +20,7 @@ interface CompanionsListProps {
 
 const CompanionsList = ({ title, companions, classNames }: CompanionsListProps) => {
     return (
-        <article className={cn('companion-list ', classNames)}>
+        <article className={cn('companion-list', classNames)}>
             <h2 className="font-bold text-3xl">{title}</h2>
 
             <Table>
